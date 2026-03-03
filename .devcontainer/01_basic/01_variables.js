d9d9fc91547dbdf7df683ebaea10644600fde9c7
Refat Mir <refatmir420@gmail.com>
2026-03-03 21:41:49 +0000

    container config done

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountID =144553
+let accountEmail ="refatmir420@gmail.com"
+var accountPassword ="12345"
+accountCity="Dhaka"
+// accountCity= "dhaka" // not allow
+accountEmail="jdjs@gmail.com"
+accountPassword="w42432"
+accountCity="gazipur"
+
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity])

3b2225d27f48bbcb80246ea765e893c5d43a155b
Refat Mir <refatmir420@gmail.com>
2026-03-04 07:44:00 +0000

    conversion of data type

---
+++ .devcontainer/01_basic/conversionOPeration.js
@@ -0,0 +1,18 @@
+let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+//  let valueINNumber = Number(score)
+//  console.log(typeof valueINNumber);
+ 
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
--- .devcontainer/01_basic/dataType.js
+++ .devcontainer/01_basic/dataType.js
@@ -1,1 +1,10 @@
 // data type
+
+"use strict"; // treat all js code as newer version
+// alert (3+3)// we are using nodejs,not browser
+console.log(3+3)// code readability should be high
+let name="refat"
+let age =25
+let married=false
+
+console.log(typeof name);

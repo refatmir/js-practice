2f678d930e6743bda4a1d8f348a58955bc704fad
Refat Mir <refatmir420@gmail.com>
2026-03-04 08:46:00 +0000

    dataType Summary

---
+++ .devcontainer/01_basic/dataType_summary.js
@@ -0,0 +1,41 @@
+//  primitive data type  //
+
+// 7 type : String,Number,Boolean,Null ,Undefined ,Symbol ,BIgInt
+
+
+const score=100;
+const totalscore =110.3;
+
+const isLOggedin=false
+const outsideTemp= null
+ let emailId;
+
+ const id =Symbol("123")
+  const anotherid =Symbol("123")
+  console.log(id=== anotherid);
+  const bigNumber=12343432434252n
+  
+
+
+
+//Reference type (non premitive type)
+//Array ,Objects,Functions//
+
+const Fruits=["apple","mango","orange"]
+
+let obj={
+    name:"Refat",
+    age:25,
+}
+
+const myFunction=function(){
+    console.log("Hello worlde");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof score);
+
+console.log(typeof Function);
+console.log(typeof Fruits);
+

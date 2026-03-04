60faf3b0db56da2de3013d6592e8b6424c633678
Refat Mir <refatmir420@gmail.com>
2026-03-04 09:40:26 +0000

    strings

---
+++ .devcontainer/01_basic/string.js
@@ -0,0 +1,18 @@
+const name="refat"
+const repoCount=50
+// console.log(name+repoCount)
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const  gameName= new String('Refatworld')
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+const newstring= gameName.substring(0,4)
+console.log(newstring);
+
+//trim
+//split
+//replace
+//includes
+//slice

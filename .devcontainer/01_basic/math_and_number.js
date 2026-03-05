06f41ea5ee1c2248eefd47817e28376c15209d53
Refat Mir <refatmir420@gmail.com>
2026-03-05 06:44:20 +0000

    math and number

---
+++ .devcontainer/01_basic/math_and_number.js
@@ -0,0 +1,26 @@
+// const score=100
+// console.log(score)
+
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1))
+
+// const otherNumber =123.89234
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds =10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math. floor(5.3));
+// console.log(Math.min(3443,453545,643,245,66545));
+
+console.log(Math.random());
+
+
+
+
+

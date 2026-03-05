d0824442cd1b09a997cb92b291dcdf30e49ea30c
Refat Mir <refatmir420@gmail.com>
2026-03-05 07:05:51 +0000

    date and time

---
+++ .devcontainer/01_basic/date.js
@@ -0,0 +1,23 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString);
+
+// let myCreatedDate=new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate=new Date("02-03-2003")
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+
+let  newdate=new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+
+
+
+
+
+
+

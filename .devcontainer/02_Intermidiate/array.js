0c0000013968af406c61be302cb71d16d3ec0932
Refat Mir <refatmir420@gmail.com>
2026-03-05 08:14:16 +0000

    array

---
+++ .devcontainer/02_Intermidiate/array.js
@@ -0,0 +1,37 @@
+// let arr=[10,20,30,40,50]
+// console.log(arr[2]);
+
+// console.log(arr);
+
+
+const arr=["apple","mango",true,31,"css","html"];
+arr[1]="react";
+arr[0]="tailwind";
+// console.log(arr.toSpliced());
+// console.log(typeof arr);
+// console.log(arr.length);
+
+const person=[]
+person['firstName']='Refat'
+person['lastName']='Mir'
+
+// console.log(person.length)
+// console.log(person);
+
+
+//delete last item
+arr.pop()
+//add firast
+arr.push("Laravel")
+//
+
+
+//array concation
+
+const arr1=[1,2,3,4,5]
+const arr2=[6,7,8,9,10]
+const arr4=[2124,543,55]
+const arr3=arr1.concat(arr2,arr4)
+arr3.splice(2,4,"refat","mir")
+
+console.log(arr3)
